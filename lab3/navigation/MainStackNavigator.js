366eadbf4da7c4652bff578413cc22b71afb14f5
salmonsamstew <37159122+salmonsamstew@users.noreply.github.com>
2019-04-22 19:27:40 -0400

    Final Project Final Commit

--- lab3/navigation/MainStackNavigator.js
+++ lab3/navigation/MainStackNavigator.js
@@ -2,34 +2,9 @@ import React from 'react';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
-<<<<<<< HEAD
-import LinksScreen from '../screens/LinksScreen';
-import Detail from '../screens/Detail';
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
-  Detail: Detail,
-=======
-import DetailScreen from '../screens/Detail';
-
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-  Detail: DetailScreen
->>>>>>> cca949b5ff178a251102000ab56290d09980f824
 }, {initialRouteName:"Home"});
 
 export default HomeStack;
-
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
-
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
-};
